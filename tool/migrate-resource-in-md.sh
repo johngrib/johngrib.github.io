7f301f92a38560069a8e5c5017f7e8d7c2d7fced
John Grib <johngrib82@gmail.com>
2022-12-04 23:06:30 +0900

    md 파일 내의 . 파일 리소스 경로 교정 셸 스크립트 추가

--- tool/migrate-resource-in-md.sh
+++ tool/migrate-resource-in-md.sh
@@ -18,13 +18,8 @@ echo "진행"
 
 TO_RESOURCE_DIR=`head $FILE_ADDR | ag -o "[A-F0-9]{2}/[A-F0-9-]{34}$"`
 
+# /resource/wiki/filename 리소스인 경우
 RESOURCE_NAME_LIST=`ag "!\[.*?\]\( *(\/resource\/wiki)\/.*?\)" "$FILE_ADDR" | ag -o "/resource/wiki/[^ ]*"`
-
-# 만약 RESOURCE_NAME_LIST 가 비어있다면 종료한다
-if [ -z "$RESOURCE_NAME_LIST" ]; then
-  echo "이주할 리소스가 없습니다."
-  exit 0
-fi
 
 mkdir -p "./resource/$TO_RESOURCE_DIR"
 
@@ -35,6 +30,21 @@ for RESOURCE_NAME in $RESOURCE_NAME_LIST; do
     echo "FROM: .$RESOURCE_FROM"
     echo "TO: .$RESOURCE_TO"
     mv ".$RESOURCE_FROM" ".$RESOURCE_TO"
+
+    perl -i -pe "s|$RESOURCE_NAME|$RESOURCE_TO|g" "$FILE_ADDR"
+done
+
+# ./filename 리소스인 경우
+RESOURCE_NAME_LIST=`ag "!\[.*?\]\( *\.\/.*?\)" "$FILE_ADDR" | ag -o "\./[^ ]*"`
+FILE_ADDR_DIR=`sed 's,\.md$,,' <<< $FILE_ADDR`
+
+for RESOURCE_NAME in $RESOURCE_NAME_LIST; do
+    RESOURCE_FROM=`sed 's,^\./,,' <<< $RESOURCE_NAME`
+    RESOURCE_FILE_NAME="${RESOURCE_NAME##*/}"
+    RESOURCE_TO="/resource/$TO_RESOURCE_DIR/$RESOURCE_FILE_NAME"
+    echo "FROM: $FILE_ADDR_DIR/$RESOURCE_FROM"
+    echo "TO: .$RESOURCE_TO"
+    mv "$FILE_ADDR_DIR/$RESOURCE_FROM" ".$RESOURCE_TO"
 
     perl -i -pe "s|$RESOURCE_NAME|$RESOURCE_TO|g" "$FILE_ADDR"
 done
