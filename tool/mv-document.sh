3d7115b1f3e7f55d3574c4f0987c99a3e38bc209
JohnGrib <johngrib82@gmail.com>
2023-04-01 22:43:14 +0900

    mv-document.sh 가 모든 링크 형식에 대응되도록 수정

--- tool/mv-document.sh
+++ tool/mv-document.sh
@@ -11,6 +11,10 @@ TO_DIR="$2"
 
 echo "Move $FROM_DIR to $TO_DIR"
 
+# 대상 디렉토리가 없다면 생성한다
+TO_DIR_PARENT=$(dirname "$TO_DIR")
+mkdir -p "$TO_DIR_PARENT"
+
 git mv "$FROM_DIR" "$TO_DIR"
 
 FROM_FILENAME=$(echo "$FROM_DIR" | sed -e 's/^_wiki\///' -e 's/\.md$//')
@@ -19,7 +23,12 @@ TO_FILENAME=$(echo "$TO_DIR" | sed -e 's/^_wiki\///' -e 's/\.md$//')
 # echo "FROM_FILENAME: $FROM_FILENAME, TO_FILENAME: $TO_FILENAME"
 # ex) FROM_FILENAME: b-tree, TO_FILENAME: algorithm/b-tree
 
-ag '(?<!\\)\[\[/?'$FROM_FILENAME'\]\]' -l \
+# 처리대상은 단순 링크: [[/path/filename]] , [[path/filename]]
+ag '(?<!\\)\[\[/?'$FROM_FILENAME'(\]\]|\#)' -l \
     | xargs sed -i '' -E \
-        's,\[\[\/?'$FROM_FILENAME'\]\],\[\[\/'$TO_FILENAME'\]\],g'
+        's,\[\[\/?'$FROM_FILENAME'(\]\]|\#),\[\[\/'$TO_FILENAME'\1,g'
 
+# 처리대상은 섹션 링크가 포함된 링크: [[path/filename#section]]
+ag '(?<!\\)\[\[/?'$FROM_FILENAME'\#([^]]+)\]\]' -l \
+    | xargs sed -i '' -E \
+        's,\[\[\/?'$FROM_FILENAME'\#([^]]+)\]\],\[\[\/'$TO_FILENAME'\#\1\]\],g'
